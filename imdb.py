6e2bd75fd1c30dfd708f0f5ae35cbc7cc7b8a899
Musa Khan <tamzid834@gmail.com>
2016-12-18 05:43:59 +0600

    some minor update.

--- imdb.py
+++ imdb.py
@@ -19,13 +19,20 @@ def get_review(url):
     except  HTTPError :
         pass
 
-    return  reviews[:-1]
+
+    return  reviews[:-1] # we are erassing the last data in the list because its not a review.
 
 
 
 
 if  __name__ == "__main__":
+
     reviews = get_review("http://www.imdb.com/title/tt0944947/reviews?ref_=tt_urv")
+
+    if reviews == None:
+        print("No review was found.")
+        exit(1)
+
 
     for index , review in enumerate(reviews):
         print(str(index),end= ".")
